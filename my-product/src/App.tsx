5b03f5bea49cd97894e1afe321f15084413aca8c
Shubham Gosain <shubham.gosain1994@gmail.com>
2026-07-26 12:29:54 -0500

    Revert "Merge pull request #21 from vansh187/SG-FrontEnd"
    This reverts commit b3ea9ad2d1142f3408a744e4f2aa1f92f3a1ee70, reversing
    changes made to 331a8dc03753a1d748e422159632d2aebedfd2b1.

--- my-product/src/App.tsx
+++ my-product/src/App.tsx
@@ -5,7 +5,6 @@ import HomePage from './components/HomePage';
 import ExploreFutureOptions from './components/ExploreFutureOptions';
 import FuturesTerminal from './components/FuturesTerminal';
 import OptionChain from './components/OptionChain';
-import OverallDashboard from './components/OverallDashboard';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/dashboard" element={<OverallDashboard />} />
         <Route path="/explore/fno" element={<ExploreFutureOptions />} />
         <Route path="/terminal/fno" element={<FuturesTerminal />} />
         <Route path="/terminal/fno/chain" element={<OptionChain />} />
